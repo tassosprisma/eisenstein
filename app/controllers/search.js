5d6f691df0e883a37131e6cccd67be57c29f2021
George Tsopanoglou <gtsop.code@gmail.com>
2015-06-20 16:28:22 +0300

    Stuff

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  queryParams: ['term'],
+  term: 'asd',
+
+  terms: function () {
+    return this.get('term').toLowerCase().split('+');
+  }.property('term'),
+
+  movies: function () {
+    var ret = [];
+    window.movies.forEach(function (movie) {
+      this.get('terms').some(function (term) {
+        if (movie.title.toLowerCase().indexOf(term) != -1) {
+          ret.push(movie);
+          return true;
+        }
+      }, this);
+    }, this);
+    return ret;
+  }.property('terms')
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  actions: {
+    search: function (value) {
+      this.transitionTo('search', {
+        queryParams: {
+          term: value.split(' ').join('+')
+        }
+      });
+    }
+  }
 });
